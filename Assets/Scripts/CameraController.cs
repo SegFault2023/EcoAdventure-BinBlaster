7d2b956405283dbacd6fbd11fad95d512445bfcc
ayan-furkan <ayan.furkan@outlook.com>
2024-01-11 13:15:09 +0300

    Finaly!

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float smoothing;
+    [Header("Following Object")]
+    public Transform player;
+
+
+    [Header("Range")]
+    public Vector2 minPosition;
+    public Vector2 maxPosition;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if(transform.position != player.position)
+        {
+            Vector3 targetPosition = new Vector3(player.position.x, player.position.y, transform.position.z);
+
+            targetPosition.x = Mathf.Clamp(targetPosition.x, minPosition.x, maxPosition.x);
+            targetPosition.y = Mathf.Clamp(targetPosition.y, minPosition.y, maxPosition.y);
+
+            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
+        }
+    }
+}
---
+++ Assets/Scripts/TrashBin.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashBin : MonoBehaviour
+{
+    Animator anim;
+
+    private Boolean can_get_trash = false;
+    private int numberOfTrashes = 0;
+
+
+    [SerializeField]
+    private int maxTrashLimit = 4;
+
+    private InventoryManager inventoryManager;
+    public ScoreManager scoreManager;
+    private GameObject selectedBin;
+
+    [SerializeField]
+    public float swapDelayInSeconds = 0.6f;
+
+   
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        inventoryManager = GameObject.Find("InventoryCanvas").GetComponent<InventoryManager>();
+        scoreManager = GameObject.Find("ScoreManager").GetComponent<ScoreManager>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(can_get_trash && Input.GetKeyDown(KeyCode.X) && (numberOfTrashes < maxTrashLimit))
+        {
+            int slotToBeRemoved = inventoryManager.getSelectedIndex();
+            anim.SetBool("ButtonPressed", true);
+
+            if(slotToBeRemoved != -1)
+            {
+                if(inventoryManager.compareTrashBinTags(this.tag, slotToBeRemoved))
+                {
+                    inventoryManager.RemoveItem(slotToBeRemoved);
+                    numberOfTrashes++;
+
+                    scoreManager.incrementScore();
+                }
+
+                else
+                {
+                    if (inventoryManager.itemSlot[slotToBeRemoved].isFull)
+                    {
+                        scoreManager.decrementScore();
+                    }
+                }
+            }
+
+        }
+
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            can_get_trash = true;
+            
+        }
+    }
+
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            can_get_trash = false;
+        }
+    }
+
+
+
+}
