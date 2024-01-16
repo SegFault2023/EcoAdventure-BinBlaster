7611e5c696d60054a379640e9dceb1d0e9d7c262
duru <derindagsd@gmail.com>
2024-01-17 00:58:18 +0300

    Created gray locked background

--- Assets/Scripts/LevelMenuSingle.cs
+++ Assets/Scripts/LevelMenuSingle.cs
@@ -7,14 +7,36 @@ using UnityEngine.UI;
 public class LevelMenuSingle : MonoBehaviour
 {
     public Button[] buttons;
+    public GameObject[] buttonMasks;
+
     private void Awake()
     {
         int unlockedLevel = PlayerPrefs.GetInt("UnlockedLevel", 1);
+
         for (int i = 0; i < buttons.Length; i++)
+        {
             buttons[i].interactable = false;
 
-        for (int i = 0; i < unlockedLevel; i++)
-            buttons[i].interactable = true;
+            // Set initial color to black and white
+            //buttons[i].image.color = Color.gray;
+
+            // Activate or deactivate the mask based on level lock
+            buttonMasks[i].SetActive(!buttons[i].interactable);
+        }
+
+        if (unlockedLevel > 0 && unlockedLevel <= buttons.Length)
+        {
+            for (int i = 0; i < unlockedLevel; i++)
+            {
+                buttons[i].interactable = true;
+
+                // Change color to white for unlocked buttons
+                //buttons[i].image.color = Color.white;
+
+                // Activate or deactivate the mask based on level lock
+                buttonMasks[i].SetActive(!buttons[i].interactable);
+            }
+        }
     }
 
     public void OpenLevel(int levelId)
