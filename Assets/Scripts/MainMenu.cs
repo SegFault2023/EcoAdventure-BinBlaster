283aa9257fa0a02bef6dca98fdaeb02c011e20e7
ayan-furkan <ayan.furkan@outlook.com>
2024-01-19 18:07:06 +0300

    lzst changes

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -12,6 +12,53 @@ public class MainMenu : MonoBehaviour
 
     public void LoginSignup()
     {
-        SceneManager.LoadSceneAsync(3);
+        SceneManager.LoadSceneAsync(12);
     }
+
+    public void Level1()
+    {
+        SceneManager.LoadSceneAsync(8);
+    }
+    public void Level2()
+    {
+        SceneManager.LoadSceneAsync(6);
+    }
+    public void Level3()
+    {
+        SceneManager.LoadSceneAsync(9);
+    }
+    public void Level4()
+    {
+        SceneManager.LoadSceneAsync(10);
+    }
+
+    public void LevelScene()
+    {
+        SceneManager.LoadSceneAsync(11);
+
+    }
+
+    public void MainMen()
+    {
+        SceneManager.LoadSceneAsync(13);
+
+    }
+
+    public void SimOrMul()
+    {
+        SceneManager.LoadSceneAsync(14);
+
+    }
+    public void SignUp()
+    {
+        SceneManager.LoadSceneAsync(15);
+
+    }
+
+    public void mpLobi()
+    {
+        SceneManager.LoadSceneAsync(0);
+
+    }
+
 }
